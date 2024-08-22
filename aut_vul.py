c0278a1545825efd65425f561da35b7414c4d056
Antonio da Rocha Dias Neto <antonio.neto@PMM.local>
2024-08-22 13:11:53 -0300

    Adiciona o projeto Scanner de Portas

--- aut_vul.py
+++ aut_vul.py
@@ -1,12 +1,13 @@
 import os
 import nmap
 import subprocess
+import logging
 
-# Função de varredura com nmap
-
+# Configura o logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def scan_with_nmap(target):
-    print(f"\nIniciando varredura Nmap no endereço: {target}")
+    logging.info(f"Iniciando varredura Nmap no endereço: {target}")
     nm = nmap.PortScanner()
     nm.scan(target, arguments='-sV -p 1-65535')
 
@@ -26,27 +27,23 @@ def scan_with_nmap(target):
                 })
     return nmap_results
 
-# Função para varredura com nikto
-
-
 def scan_with_nikto(target):
-    print(f"\nIniciando varredura Nikto no endereço: {target}")
+    logging.info(f"Iniciando varredura Nikto no endereço: {target}")
     nikto_results_file = 'nikto_results.txt'
-    nikto_command = f'perl C:\\nikto\\program\\nikto.pl -h {
-        target} -output {nikto_results_file}'
+    nikto_command = f'perl [CAMINHO_PARA_NIKTO]\\nikto.pl -h {target} -output {nikto_results_file}'
     subprocess.run(nikto_command, shell=True, check=True)
+
+    if not os.path.exists(nikto_results_file):
+        logging.error(f"Arquivo de resultados do Nikto não encontrado: {nikto_results_file}")
+        return ""
 
     with open(nikto_results_file, 'r', encoding='utf-8') as file:
         nikto_results = file.read()
 
     return nikto_results
 
-# Função principal de execução de varredura
-
-
 def main():
-    prefix = input(
-        "Digite o prefixo (primeiros três octetos) do intervalo de IP: ")
+    prefix = input("Digite o prefixo (primeiros três octetos) do intervalo de IP: ")
     start = int(input("Digite o início do intervalo do último octeto: "))
     end = int(input("Digite o fim do intervalo do último octeto: "))
 
@@ -54,31 +51,23 @@ def main():
         target = f"{prefix}.{i}"
 
         try:
-            # Realizar a varredura com nmap
             nmap_results = scan_with_nmap(target)
-
-            # Exibe resultado do Nmap
-            print(f"\nResultados do Nmap para o alvo {target}:")
+            logging.info(f"Resultados do Nmap para o alvo {target}:")
             if not nmap_results:
-                print(f"Sem resultados Nmap para o alvo {target}.")
+                logging.info(f"Sem resultados Nmap para o alvo {target}.")
             else:
                 for result in nmap_results:
-                    print(f"Host: {result['host']}, Port: {result['port']}, Protocol: {result['protocol']}, "
-                          f"State: {result['state']}, Service: {
-                              result['name']}, Product: {result['product']}, "
-                          f"Version: {result['version']}")
+                    logging.info(f"Host: {result['host']}, Port: {result['port']}, Protocol: {result['protocol']}, "
+                                 f"State: {result['state']}, Service: {result['name']}, Product: {result['product']}, "
+                                 f"Version: {result['version']}")
 
-            # Realiza a varredura com Nikto
-            print("\nExecutando varredura com Nikto...")
+            logging.info("Executando varredura com Nikto...")
             nikto_results = scan_with_nikto(target)
-
-            # Exibe resultado do Nikto
-            print(f"\nResultados do Nikto para o alvo {target}:")
-            print(nikto_results)
+            logging.info(f"Resultados do Nikto para o alvo {target}:")
+            logging.info(nikto_results)
 
         except Exception as e:
-            print(f"Ocorreu um erro ao processar o alvo {target}: {e}")
-
+            logging.error(f"Ocorreu um erro ao processar o alvo {target}: {e}")
 
 if __name__ == "__main__":
     main()
